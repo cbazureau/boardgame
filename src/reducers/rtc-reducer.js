0f67635d0c4323247b4a5cf5275622bad2cfdea5
Cédric Bazureau <cedric.bazureau@renault.com>
2020-10-20 12:12:23 +0200

    small changes

--- src/reducers/rtc-reducer.js
+++ src/reducers/rtc-reducer.js
@@ -9,6 +9,7 @@ const initalState = {
 const rtcReducer = (state = initalState, action) => {
   switch (action.type) {
     case 'ADD_ROOM':
+      if(state.rooms.includes(action.room)) return state;
       return {...state, rooms: [...state.rooms, action.room]};
     case 'SET_VIDEO':
       return {...state, isVideoEnabled: action.isVideoEnabled};
